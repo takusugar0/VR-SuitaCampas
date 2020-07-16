88f7286b2c757f7026efbcac7e0cf4952dd3b774
takusugar0 <takusugar9@gmail.com>
2020-07-16 12:33:29 +0900

    camera

---
+++ SceneChange/ToM3.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+// public class scene : MonoBehaviour {
+// void OnTriggerEnter (Collider other)
+// {
+// SceneManager.LoadScene("M3");
+// }
+
+// }
+public class ToM3 : MonoBehaviour
+{
+    // オブジェクトと接触した時に呼ばれるコールバック
+    void OnCollisionEnter (Collision hit)
+    {
+        // 接触したオブジェクトのタグが"Player"の場合
+        if (hit.gameObject.CompareTag ("Player")) {
+
+
+            SceneManager.LoadScene("M3");
+        }
+    }
+}
